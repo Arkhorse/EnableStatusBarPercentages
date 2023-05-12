abf8d14898df8126c4e0e021c42db9c4e7ec74f8
Arkhorse <theillusivemman@gmail.com>
2023-05-12 03:18:44 -0300

    upd to current melonloader injection

--- EnableStatusBarPercentagesMod.cs
+++ EnableStatusBarPercentagesMod.cs
@@ -1,10 +1,12 @@
 ﻿using MelonLoader;
-using UnityEngine;
 
-namespace EnableStatusBarPercentages {
-	internal class EnableStatusBarPercentagesMod : MelonMod {
-		public override void OnApplicationStart() {
-			Debug.Log($"[{Info.Name}] version {Info.Version} loaded!");
-		}
+namespace EnableStatusBarPercentages
+{
+	internal class EnableStatusBarPercentagesMod : MelonMod 
+	{
+        public override void OnInitializeMelon()
+        {
+            
+        }
 	}
 }
